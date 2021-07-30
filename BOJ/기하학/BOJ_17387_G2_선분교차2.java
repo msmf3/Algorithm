378a9138424aee64b9525726332bae0eec2c90c9
김윤성 <msmf3@naver.com>
2021-07-30 17:46:36 +0900

    Daily Algorithm 21.07.30

---
+++ BOJ/기하학/BOJ_17387_G2_선분교차2.java
@@ -0,0 +1,86 @@
+package 기하학;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/*
+ * CCW(Counter Clock Wise) : https://johoonday.tistory.com/102 
+ * 코드 참고 : https://johoonday.tistory.com/107
+ */
+
+public class BOJ_17387_G2_선분교차2 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		Dot A = new Dot(Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken()));
+		Dot B = new Dot(Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken()));
+		st = new StringTokenizer(br.readLine());
+		Dot C = new Dot(Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken()));
+		Dot D = new Dot(Long.parseLong(st.nextToken()), Long.parseLong(st.nextToken()));
+		if(isCross(A, B, C, D)) {
+			System.out.println(1);
+		}
+		else {
+			System.out.println(0);
+		}
+	}
+	
+	private static int CCW(Dot d1, Dot d2, Dot d3) {
+		long temp = (d2.x - d1.x) * (d3.y - d1.y) - (d2.y - d1.y) * (d3.x - d1.x);
+		if(temp > 0) {
+			return 1;
+		}
+		else if(temp < 0) {
+			return -1;
+		}
+		else {
+			return 0;
+		}
+	}
+	
+	private static boolean isCross(Dot d1, Dot d2, Dot d3, Dot d4) {
+		int ccw1 = CCW(d1, d2, d3) * CCW(d1, d2, d4);
+		int ccw2 = CCW(d3, d4, d1) * CCW(d3, d4, d2);
+		// 네 점이 직선상에 있는 경우
+		if(ccw1 == 0 && ccw2 == 0) {
+			if(isOverlap(d1, d2, d3) || isOverlap(d1, d2, d4) || isOverlap(d3, d4, d1) || isOverlap(d3, d4, d2)) return true;
+			return false;
+		}
+		// ccw1, ccw2 둘다 시계(-1) 반시계(1) 방향인 경우
+		// 교차하는거라고 볼 수 있음
+		// <= 0 인 이유는 세 점이 한 직선상에 있는 경우까지 고려
+		return (ccw1 <= 0) && (ccw2 <= 0);
+	}
+	
+	// 네 점이 직선상에 있는 경우 선분이 겹치는지 확인
+	private static boolean isOverlap(Dot d1, Dot d2, Dot d3) {
+		if(d1.x < d2.x) {
+			if(d3.x < d1.x || d2.x < d3.x) return false;
+		}
+		else if(d2.x < d1.x) {
+			if(d3.x < d2.x || d1.x < d3.x) return false;
+		}
+		// x가 같은 경우 y를 비교
+		else {
+			if(d1.y < d2.y) {
+				if(d3.y < d1.y || d2.y < d3.y) return false;
+			}
+			else if(d2.y < d1.y) {
+				if(d3.y < d2.y || d1.y < d3.y) return false;
+			}
+		} 
+		return true;
+	}
+	
+	static class Dot {
+		long x, y;
+		
+		public Dot(long x, long y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
+}
