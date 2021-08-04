f3e4102b5686883768e9f58731c655c4d42af44f
김윤성 <msmf3@naver.com>
2021-08-04 16:01:25 +0900

    Daily Algorithm 21.08.03

---
+++ BOJ/DFS와BFS/BOJ_9328_G1_열쇠.java
@@ -0,0 +1,152 @@
+package DFS와BFS;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class BOJ_9328_G1_열쇠 {
+	static int h, w, key_bitset, ans;
+	static ArrayList<Point> spList;
+	static ArrayList<Door> doorList;
+	static char map[][];
+	static boolean visited[][];
+	static Queue<Point> q;
+	static int dr[] = {-1, 1, 0, 0};
+	static int dc[] = {0, 0, -1, 1};
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		int T = Integer.parseInt(br.readLine());
+		for(int tc = 1; tc <= T; tc++) {
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			h = Integer.parseInt(st.nextToken());
+			w = Integer.parseInt(st.nextToken());
+			map = new char[h][w];
+			visited = new boolean[h][w];
+			q = new LinkedList<>();
+			spList = new ArrayList<>();
+			doorList = new ArrayList<>();
+			ans = 0;
+			for(int i = 0; i < h; i++) {
+				String line = br.readLine();
+				for(int j = 0; j < w; j++) {
+					map[i][j] = line.charAt(j);
+					if(i == 0 || j == 0 || i == h-1 || j == w-1) {
+						if(map[i][j] == '$') {
+							// 가장자리가 $인 경우 바로 ++ans
+							++ans;
+							map[i][j] = '.';
+							spList.add(new Point(i, j));
+						}
+						else if(map[i][j] == '.') {
+							// 시작점이 될 수 있는 지점
+							spList.add(new Point(i, j));
+						}
+						if('A' <= map[i][j] && map[i][j] <= 'Z') {
+							doorList.add(new Door(i, j, map[i][j] - 'A'));
+						}
+					}
+				}
+			}
+			// bitset에 열쇠 추가
+			String key = br.readLine();
+			key_bitset = 0;
+			if(key != "0") {
+				for(int i = 0; i < key.length(); i++) {
+					int num = key.charAt(i) - 'a';
+					key_bitset |= (1 << num);
+				}
+			}
+			// 문들 중 시작점이 될 수 있는 경우 추가
+			for(int i = 0; i < doorList.size(); i++) {
+				Door door = doorList.get(i);
+				if(door.r == 0 || door.c == 0 || door.r == h-1 || door.c == w-1) {
+					if((key_bitset & (1 << door.num)) != 0) {
+						spList.add(new Point(door.r, door.c));
+					}
+				}
+			}
+			// 시작점부터 bfs 실행
+			for(int i = 0; i < spList.size(); i++) {
+				bfs(spList.get(i));
+			}
+			sb.append(ans + "\n");
+		}
+		System.out.print(sb.toString());
+	}
+	
+	private static void bfs(Point sp) {
+		q.add(sp);
+		visited[sp.r][sp.c] = true; 
+		
+		while(!q.isEmpty()) {
+			Point p = q.poll();
+			for(int k = 0; k < 4; k++) {
+				int nr = p.r + dr[k];
+				int nc = p.c + dc[k];
+				if(nr < 0 || h <= nr || nc < 0 || w <= nc) continue;
+				if(visited[nr][nc] || map[nr][nc] == '*') continue;
+				
+				char c = map[nr][nc];
+				if(c != '.') {	
+					// 문서 or 열쇠 or 문을 만난 경우
+					if(c == '$') {
+						// 문서를 만난 경우
+						++ans;
+					}
+					else if('A' <= c && c <= 'Z') {
+						// 문을 만난 경우
+						int door = c - 'A';
+						if((key_bitset & (1 << door)) == 0) {
+							// 해당 열쇠가 없는 경우
+							doorList.add(new Door(nr, nc, door));
+							continue;
+						}
+					}
+					else {
+						// 열쇠를 만난 경우
+						// 비트셋에 열쇠를 추가하고 기존에 못들어간 문을 확인
+						int key = c - 'a';
+						key_bitset |= (1 << key);
+						for(int i = 0; i < doorList.size(); i++) {
+							Door door = doorList.get(i);
+							if(key == door.num) {
+								map[door.r][door.c] = '.'; 
+								q.add(new Point(door.r, door.c));
+								visited[door.r][door.c] = true;
+							}
+						}
+					}
+					// 다시 체크하지 않도록 빈 공간으로 만듦
+					map[nr][nc] = '.';
+				}
+				q.add(new Point(nr, nc));
+				visited[nr][nc] = true;
+			}
+		}
+	}
+	
+	static class Point {
+		int r, c;
+		
+		public Point(int r, int c) {
+			this.r = r;
+			this.c = c;
+		}
+	}
+	
+	static class Door {
+		int r, c, num;
+		
+		public Door(int r, int c, int num) {
+			this.r = r;
+			this.c = c;
+			this.num = num;
+		}
+	}
+}
